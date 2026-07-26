4664f282de88f7a0c819f50ae90a68cdaba3e8ba
Khan Trần <k.coder.282@gmail.com>
2026-07-26 17:46:20 +0700

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 import dry from 'drycms';
 
 export default defineConfig({
-	integrations: [dry({ storage: { kind: 'github' } })],
+	integrations: [dry({ storage: { kind: 'local' } })],
 });
